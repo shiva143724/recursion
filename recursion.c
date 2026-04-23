27a8e2bee5c26fe71881d3ca69604f927cb262d3
shiva143724 <keerthishivaganesh34@gmail.com>
2026-04-23 16:17:01 +0530

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int output(int a,int sum)
+{
+    if(a==0)
+    {
+        return sum;
+    }
+    int dig=a%10;
+    sum=sum+dig;
+    a=a/10;
+    output (a,sum);
+}
+int main()
+{
+    int a;
+    printf("enter the value");
+    scanf("%d",&a);
+    int res=output(a,0);
+    printf("the sum is %d",res);
+}
